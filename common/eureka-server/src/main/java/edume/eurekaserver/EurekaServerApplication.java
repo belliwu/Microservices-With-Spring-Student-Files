2293da23afa818908c881a838c0479c4ad2f98b6
belliwu <belliwu56@gmail.com>
2018-10-16 18:03:31 +0800

    bug fix eureka server

--- common/eureka-server/src/main/java/edume/eurekaserver/EurekaServerApplication.java
+++ common/eureka-server/src/main/java/edume/eurekaserver/EurekaServerApplication.java
@@ -2,11 +2,14 @@ package edume.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaServerApplication {
-
-	public static void main(String[] args) {
+@EnableEurekaServer
+public class EurekaServerApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
